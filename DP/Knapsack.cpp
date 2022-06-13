b9db9d5cc158204b87963793762fb84654291608
Krishna Sahay <krishnasahay.27@gmail.com>
2022-06-14 02:54:28 +0530

    format files
    Previously, these files hadn't been formatted by clang-format. Now, all files are properly formatted and will appear neater
    
    This is a sample footer message

--- DP/Knapsack.cpp
+++ DP/Knapsack.cpp
@@ -1,55 +1,53 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-
- // } Driver Code Ends
+// } Driver Code Ends
 class Solution
 {
-    public:
-    #define INF INT_MAX ;
-    int knapSack(int W, int wt[], int val[], int n) 
-    { 
-       // Your code here
-        if(n == 0 || W == 0)
+public:
+#define INF INT_MAX;
+    int knapSack(int W, int wt[], int val[], int n)
+    {
+        // Your code here
+        if (n == 0 || W == 0)
             return 0;
-        
+
         int include = -INF;
-        int exclude = knapSack(W,wt,val,n-1);
-        
-        if(wt[n-1] <= W)
-            include = val[n-1] + knapSack(W-wt[n-1],wt,val,n-1);
-            
-        return max(include,exclude);    
+        int exclude = knapSack(W, wt, val, n - 1);
+
+        if (wt[n - 1] <= W)
+            include = val[n - 1] + knapSack(W - wt[n - 1], wt, val, n - 1);
+
+        return max(include, exclude);
     }
 };
 
 // { Driver Code Starts.
 
 int main()
- {
-    //taking total testcases
+{
+    // taking total testcases
     int t;
-    cin>>t;
-    while(t--)
+    cin >> t;
+    while (t--)
     {
-        //reading number of elements and weight
+        // reading number of elements and weight
         int n, w;
-        cin>>n>>w;
-        
+        cin >> n >> w;
+
         int val[n];
         int wt[n];
-        
-        //inserting the values
-        for(int i=0;i<n;i++)
-            cin>>val[i];
-        
-        //inserting the weights
-        for(int i=0;i<n;i++)
-            cin>>wt[i];
+
+        // inserting the values
+        for (int i = 0; i < n; i++)
+            cin >> val[i];
+
+        // inserting the weights
+        for (int i = 0; i < n; i++)
+            cin >> wt[i];
         Solution ob;
-        //calling method knapSack()
-        cout<<ob.knapSack(w, wt, val, n)<<endl;
-        
+        // calling method knapSack()
+        cout << ob.knapSack(w, wt, val, n) << endl;
     }
-	return 0;
-}  // } Driver Code Ends
+    return 0;
+} // } Driver Code Ends
