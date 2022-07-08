d164d747c3916242c88a8d981a69f6bf0630b6db
Krishna Sahay <krishnasahay.27@gmail.com>
2022-07-08 20:41:52 +0530

    add file longest common supersequence as tabular and memoization

--- DP/shortestCommonSupersequence.cpp
+++ DP/shortestCommonSupersequence.cpp
@@ -16,36 +16,40 @@ public:
     // Function to find length of shortest common supersequence of two strings.
     int lcs(string X, string Y, int m, int n)
     {
-        if (m == 0 || n == 0)
-            return 0;
+        if (m == 0)
+            return n;
+
+        else if (n == 0)
+            return m;
 
         if (dp[m][n] != -1)
             return dp[m][n];
 
         if (X[m - 1] == Y[n - 1])
-            return 1 + lcs(X, Y, m - 1, n - 1);
+            return dp[m][n] = 1 + lcs(X, Y, m - 1, n - 1);
 
-        return dp[m][n] = max(lcs(X, Y, m - 1, n), lcs(X, Y, m, n - 1));
+        return dp[m][n] = 1 + min(lcs(X, Y, m - 1, n), lcs(X, Y, m, n - 1));
     }
 
     int shortestCommonSupersequence(string X, string Y, int m, int n)
     {
         // code here
-        dp.resize(m + 1, vector<int>(n + 1, 0));
+        dp.resize(m + 1, vector<int>(n + 1, -1));
         for (int i = 0; i <= m; i++)
-            dp[i][0] = 0;
+            dp[i][0] = i;
 
         for (int i = 0; i <= n; i++)
-            dp[0][i] = 0;
+            dp[0][i] = i;
 
         for (int i = 1; i <= m; i++)
             for (int j = 1; j <= n; j++)
                 if (X[i - 1] == Y[j - 1])
                     dp[i][j] = 1 + dp[i - 1][j - 1];
                 else
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                    dp[i][j] = 1 + min(dp[i - 1][j], dp[i][j - 1]);
 
-        return (m - dp[m][n] + n);
+        // lcs(X,Y,m,n);
+        return dp[m][n];
     }
 };
 
