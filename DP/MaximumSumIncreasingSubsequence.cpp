01f79eaff98bdb7f6f20cc1e376f1eb8886a24d6
Krishna Sahay <krishnasahay.27@gmail.com>
2022-07-09 18:47:31 +0530

    made code more elegant

--- DP/MaximumSumIncreasingSubsequence.cpp
+++ DP/MaximumSumIncreasingSubsequence.cpp
@@ -8,7 +8,7 @@ class Solution
 
 private:
     int N;
-    int **t;
+    int **dp;
 
 public:
     int checkSumSub(int arr[], int n, int preIdx)
@@ -16,49 +16,52 @@ public:
         if (n == 0)
             return 0;
 
-        if (t[n][preIdx] != -1)
-            return t[n][preIdx];
+        if (dp[n][preIdx] != -1)
+            return dp[n][preIdx];
 
         else if (preIdx == N || arr[n - 1] < arr[preIdx])
-            return t[n][preIdx] = max((arr[n - 1] + checkSumSub(arr, n - 1, n - 1)), checkSumSub(arr, n - 1, preIdx));
+        {
+            int inclusion = arr[n - 1] + checkSumSub(arr, n - 1, n - 1);
+            int exclusion = checkSumSub(arr, n - 1, preIdx);
+            return dp[n][preIdx] = max(inclusion, exclusion);
+        }
 
         else
-            return t[n][preIdx] = checkSumSub(arr, n - 1, preIdx);
+            return dp[n][preIdx] = checkSumSub(arr, n - 1, preIdx);
     }
+
     int maxSumIS(int arr[], int n)
     {
         // Your code goes here
-        /*int **t;
-        t = new int*[n+1];
-        for(int i = 0; i < n+1; i++)
-            t[i] = new int[n+1];
+        int **dp;
+        dp = new int *[n + 1];
+        for (int i = 0; i < n + 1; i++)
+            dp[i] = new int[n + 1];
 
-        this->t = t;
+        this->dp = dp;
         this->N = n;
 
-        for (int i = 0; i < n+1; i++)
+        for (int i = 0; i < n + 1; i++)
             for (int j = 0; j < n + 1; j++)
-                t[i][j] = -1;
+                dp[i][j] = -1;
 
         int preIdx = n;
-        checkSumSub(arr,n,preIdx);
-        return this->t[n][preIdx];*/
-        int dp[n + 1];
+        checkSumSub(arr, n, preIdx);
+        return this->dp[n][preIdx];
+        /*int dp[n+1];
         dp[0] = 0;
         int maxSum = arr[0];
 
-        for (int i = 1; i <= n; i++)
-        {
-            dp[i] = arr[i - 1];
+        for(int i = 1; i <=n; i++)
+           {
+               dp[i] = arr[i-1];
+               for(int j = 1; j < i; j++)
+                   if(arr[j-1] < arr[i-1])
+                    dp[i] = max(dp[i], dp[j] + arr[i-1]);
 
-            for (int j = 1; j < i; j++)
-            {
-                if (arr[j - 1] < arr[i - 1])
-                    dp[i] = max(dp[i], dp[j] + arr[i - 1]);
-                maxSum = max(maxSum, dp[i]);
-            }
-        }
-        return maxSum;
+               maxSum = max(maxSum,dp[i]);
+           }
+           return maxSum;*/
     }
 };
 
